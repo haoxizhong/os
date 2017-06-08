bf5cb3912cc85e45ba25482105fc4955fa5c7c19
zhonghaoxi <zhonghaoxi@yeah.net>
2017-06-08 13:53:36 +0800

    gg

---
+++ nachos/proj4/proj4/blockchaindb_java/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac src/main/zhx/Main.java
---
+++ nachos/proj4/proj4/blockchaindb_java/src/main/zhx/Main.java
@@ -0,0 +1,11 @@
+package zhx.main;
+
+import zhx.hash.Hash;
+
+public class Main
+{
+	public static void main(String []args)
+	{
+		System.out.println(Hash.checkHash("12346"));
+	}
+}
